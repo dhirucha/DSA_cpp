4ba9e773b299dac160b0eb136fa09b2a2a7a4b15
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-10-22 22:48:07 +0530

    DSA in C++_pattern-problems

---
+++ code.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    string fullName = "Dheeraj chaubey";
+
+    int age = 20;
+
+    cout<<sizeof(age) <<endl;
+
+    cout<<sizeof(fullName);
+
+    // float PI = 3.14f;
+    // cout<<PI;
+
+
+    //Typecasting
+    // char grade = 'A';
+
+    // int value = grade;
+    // cout<<value;
+
+    // double price = 100.90;
+    //  int newPrice = (int)price;
+    //  cout<<newPrice;
+
+
+    //Input
+    // int age;
+
+    // cout<<"Enter the age:";
+    // cin>>age;
+
+    // cout<<"your age is:"<<age;
+    
+    //unary operator
+    // int a = 10;
+     
+    //  int b = ++a;
+    //  cout<<b <<endl;
+
+    
+    
+    return 0;
+}
---
+++ pattern.cpp
@@ -0,0 +1,74 @@
+// pattern-1
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+
+//     int n = 4;
+
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<'*' <<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// pattern-2
+
+//  #include<iostream>
+
+//  using namespace std;
+//  int main(){
+//      int n = 4;
+//      for(int i=0;i<n;i++){
+//         char ch = 'A';
+//         for(int j=0; j<n; j++){
+//             cout<< ch <<" ";
+//             ch = ch + 1;
+//         }
+//         cout<<endl;
+
+//      }
+//      return 0;
+//  }
+
+// pattern-3
+
+// #include <iostream>
+
+// using namespace std;
+// int main()
+// {
+//     int n = 3;
+//     int num = 1;
+//     for(int i=0; i<n; i++){
+        
+//         for(int j=0; j<n; j++){
+//             cout<<num <<" ";
+//             num++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//pattern-4
+
+#include <iostream>
+
+ using namespace std;
+ int main()
+ {
+     int n = 3;
+     for (int i = 0; i < n; i++)
+     {
+        
+     }
+     
+
+     return 0;
+ } 
