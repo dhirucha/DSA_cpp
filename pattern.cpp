1327b5bc08083912012a1ca0be55286d6eda9456
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-10-22 22:53:08 +0530

    pattern_problems

--- pattern.cpp
+++ pattern.cpp
@@ -64,9 +64,14 @@
  int main()
  {
      int n = 3;
+     char ch = 'A';
      for (int i = 0; i < n; i++)
      {
-        
+        for(int j=0; j<n; j++){
+            cout<<ch <<" ";
+            ch++;
+        }
+        cout<<endl;
      }
      
 
