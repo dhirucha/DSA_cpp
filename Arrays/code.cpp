f5949f34613ebfbf569edaaae5305cdaa3edd996
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-10-27 17:28:04 +0530

    Array_practice

---
+++ Arrays/code.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+
+// int linearSearch(int arr[], int sz, int target){
+//     for(int i=0; i<sz; i++){
+//         if(arr[i] == target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+
+//reverse an array
+// void reverseArray(int arr[], int sz){
+//     int start = 0, end = sz-1;
+
+//     while(start < end){
+//         swap(arr[start], arr[end]);
+//         start++;
+//         end--;
+
+//     }
+// }
+
+//calulate sum and product of an array
+// void calculateSumAndProduct(int arr[], int size, int& sum, int& product) {
+//     sum = 0;
+//     product = 1;
+    
+//     for (int i = 0; i < size; i++) {
+//         sum += arr[i];
+//         product *= arr[i];
+//     }
+// }
+
+
+
+
+
+int main()
+{   
+    //array
+    // int size = 5;
+    // int marks[size];
+    
+    // for(int i=0; i<size; i++){
+    //     cin>>marks[i];
+    // }
+
+    // //loops
+    // for(int i=0; i<size; i++){
+    //     cout<< "Entered array:"<< marks[i] << endl;
+    // }
+
+
+
+    // finding smallest mumber in array
+    // int nums[] = {5, 15, 60, 70, 67};
+    // int size = 5;
+
+    // int smallest = INT8_MAX;
+
+    // for(int i=0; i<size; i++){
+    //     if(nums[i] < smallest){
+    //         smallest = nums[i];
+    //     }
+    // }
+
+
+    // cout<< "smallest = " << smallest << endl;
+
+
+
+
+    //finding largest number
+    // int nums[] = {5, 15, 60, 70, 67};
+    // int size = 5;
+
+    // int index;
+
+    // int largest = INT8_MIN;
+
+    // for(int i=0; i<size; i++){
+    //     if(nums[i] > largest){
+    //         largest = nums[i];
+    //     }
+    // if(largest == nums[i]){
+    //     index = i;
+    // }
+
+    // }
+
+
+    // cout<< "Largest index = " << index << endl;
+
+
+
+    //linear search
+
+    // int arr[] = {4,5,6,7,8};
+    // int sz = 5;
+    // int target = 8;
+
+    // cout<< linearSearch(arr, sz,target) << endl;
+
+
+
+    //reverse an array
+
+    // reverseArray(arr, sz);
+
+    // for(int i=0; i<sz; i++){
+    //     cout<< arr[i] << " ";
+    // }
+
+    // cout<< endl;
+
+
+    // int arr[] = {1, 2, 3, 4, 5};
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    // int sum, product;
+
+    // calculateSumAndProduct(arr, size, sum, product);
+
+    // cout << "Sum: " << sum << endl;
+    // cout << "Product: " << product << endl;
+
+    
+
+    return 0;
+}
