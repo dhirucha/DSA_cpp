00ba617385ad06e7c6a987f240b6cc6c00775022
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-10-28 18:42:24 +0530

    vectors-problems

---
+++ Vectors/code.cpp
@@ -0,0 +1,48 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+// int main()
+// {
+
+//     // vector<int> vec;
+    
+//     // // for(int i : vec){
+//     // //     cout<< i << endl;
+//     // // }
+
+
+
+//     // vec.push_back(23);
+//     // vec.push_back(24);
+//     // vec.push_back(25);
+
+    
+
+//     //     cout<< "After push_back size =" << vec.size() << endl;
+
+//     //     //vec.pop_back();
+
+//     // cout << vec.at(1)<< endl;
+
+//     vector<int> vec;
+
+//     vec.push_back(0);
+//     vec.push_back(1);
+
+//     vec.push_back(2);
+//     vec.push_back(3);
+//     vec.push_back(4);
+
+//     cout<< "size of vector :"<< vec.size() << endl;
+//     cout<< "capacity of vector :"<< vec.capacity() << endl;
+
+
+
+
+//     return 0;
+// }
+
+
+//single number
+
